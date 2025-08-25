d9274ed0caac3279c22ce652a29628c6451f2129
shoikotsazzad <mdshoikot202123@gmail.com>
2025-08-25 14:17:34 +0600

    [Mod ule-67: Complete] Users data collection done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hpxccpu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,8 +28,18 @@ async function run() {
     await client.connect();
 
 
+    const userCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
+
+
+    //users related api
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
 
     app.get('/menu', async(req,res) =>{
         const result = await menuCollection.find().toArray();
@@ -38,6 +48,28 @@ async function run() {
     app.get('/reviews', async(req,res) =>{
         const result = await reviewCollection.find().toArray();
         res.send(result);
+    })
+
+    //carts collection
+
+    app.get('/carts', async(req, res)=>{
+      const email = req.query.email;
+      const query = {email: email}
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/carts', async(req, res)=> {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
